218c1bc7b45a088d87be38b15c3bd07e67f82d59
Kurulus571 <kurtulus571@radyobilge.com>
2021-02-16 07:59:03 +0300

    Create init.py

---
+++ Loops/init.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+c=1
+while c<=10:
+    print(str(n)+" x "+str(c)+" = "+str((n*c)))
+    c = c+1
